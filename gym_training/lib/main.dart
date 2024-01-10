e8fbe37d5013d6c01d41865f67ac0b2c0738f133
Pedro Bueno <pedroarcbueno@gmail.com>
2024-01-10 17:11:44 -0300

    Finishing enviroment settings

--- gym_training/lib/main.dart
+++ gym_training/lib/main.dart
@@ -4,9 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:gym_training/bindings.dart';
-import 'package:gym_training/firebase_options.dart';
 import 'package:gym_training/pages/splash/splash_page.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 // ignore: depend_on_referenced_packages
 import 'package:get_storage/get_storage.dart';
 
