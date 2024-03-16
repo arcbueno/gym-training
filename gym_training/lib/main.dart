01e38853323af5c3d02b588c5a2d3622f9959462
Pedro Bueno <pedroarcbueno@gmail.com>
2024-03-16 16:36:00 -0300

    adding comment

--- gym_training/lib/main.dart
+++ gym_training/lib/main.dart
@@ -14,7 +14,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   await Firebase.initializeApp(
-    name: 'gym_training',
+    // Comment when generating release
+    // name: 'gym_training',
     options: FirebaseOptions(
       apiKey: const String.fromEnvironment('apiKey'),
       appId: Platform.isAndroid
